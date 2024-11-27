93e9f2cf14ed06c46f80cb7487d84bfba1f4008f
Edison Furusato <edison.oda@hotmail.com>
2024-11-27 16:56:40 -0300

    first commit

---
+++ src/app/routes.ts
@@ -0,0 +1,10 @@
+import {Routes} from '@angular/router';
+
+const routeConfig: Routes = [
+    // {
+    //     path: '',
+    //     component: HomeComponent,
+    //     title: 'Home page',
+    // },
+];
+export default routeConfig;
