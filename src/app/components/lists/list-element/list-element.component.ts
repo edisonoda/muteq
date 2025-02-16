9973f1788a63f5d00f34512f702c1fcb60389a7b
Edison Furusato <edison.oda@hotmail.com>
2025-02-15 22:05:21 -0300

    Fix list element without image

--- src/app/components/lists/list-element/list-element.component.ts
+++ src/app/components/lists/list-element/list-element.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, Input } from '@angular/core';
 import { CommonModule, NgOptimizedImage, provideImgixLoader } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
@@ -11,13 +11,17 @@ import { Listable } from 'src/app/interfaces/listable';
   styleUrls: ['./list-element.component.css'],
   providers: [
     // provideImgixLoader('http://localhost:4200/'),
-  ]
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ListElementComponent {
   private _element: Listable = { id: -1, name: "", description: "" };
   public get element() { return this._element; }
   @Input()
-  public set element(e: Listable) { this._element = e; }
+  public set element(e: Listable) {
+    this._element = e;
+    this.cdr.detectChanges();
+  }
 
-  constructor() { }
+  constructor(private cdr: ChangeDetectorRef) { }
 }
